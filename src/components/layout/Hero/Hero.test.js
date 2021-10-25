994f993a9b6ce3d4f5effdd5e445657acc1a9aa8
helena <helena@gmail.com>
2021-10-25 08:07:59 +0200

    first test added

---
+++ src/components/layout/Hero/Hero.test.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import Hero from './Hero';
+
+describe('Component Hero', () => {
+    
+  it('should render without crashing', () => {
+    const component = shallow(<Hero titleText='Lorem ipsum' />);
+    console.log(component.debug());
+    expect(component).toBeTruthy();
+  });
+
+  it('should throw error without required props', () => {
+    expect(() => shallow(<Hero />)).toThrow();
+  });
+  it('should render correct title', () => {
+      const expectedTitle ='Lorem Ipsum';
+      const component = shallow(<Hero titleText={expectedTitle} />)
+
+      const renderedTitle = component.find('.title').text();
+      expect(renderedTitle).toEqual(expectedTitle);
+  })
+});
